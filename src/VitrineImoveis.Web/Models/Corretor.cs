0e4da2637ecba4d2c0c7df48d622e789a5795353
maicon-theodoro <maicon.vitorsilva@hotmail.com>
2025-09-06 04:17:15 -0300

    Tabela dos Corretor implementada para o BD

---
+++ src/VitrineImoveis.Web/Models/Corretor.cs
@@ -0,0 +1,32 @@
+// Local: VitrineImoveis.Web/Models/TipoImovel.cs
+
+using System.ComponentModel.DataAnnotations;
+
+namespace VitrineImoveis.Web.Models
+{
+    // Esta classe será a tabela "Corretores" no banco de dados"
+    public class Corretor
+    {
+        [Key]
+        public int Id { get; set; } // Chave primaria
+
+        [Required(ErrorMessage = "O nome do corretor é obrigatório.")]
+        [StringLength(150)]
+        public string Nome { get; set; } = null!; // Nome do corretor
+
+        [Required(ErrorMessage = "O e-mail do corretor é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Por favor, insira um e-mail válido.")]
+        public string Email { get; set; } = null!; // E-mail do corretor
+
+        [StringLength(20)]
+        public string? Telefone { get; set; } // Telefone do corretor (opcional) o "?" indica que a propriedade pode ser nula (nullable)
+
+        [StringLength(20)]
+        public string? Creci { get; set; } // Creci do corretor (opcional)
+
+        public string? UrlFoto { get; set; } // URL da foto do corretor (opcional
+
+        [StringLength(1000)]
+        public string? Bio { get; set; } // Biografia do corretor (opcional)
+    }
+}
