05a65a780c757dfbf491436b459480f26ed0b406
maicon-theodoro <maicon.vitorsilva@hotmail.com>
2025-09-06 14:01:45 -0300

    Correção de bugs no lead.cs em models

--- src/VitrineImoveis.Web/Models/Lead.cs
+++ src/VitrineImoveis.Web/Models/Lead.cs
@@ -1,28 +1,36 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public enum OrigemLead { FormularioContatoGeral, PaginaDeImovel, Outro }
-
-public class Lead
+// CORREÇÃO 1: A palavra correta é 'namespace'
+namespace VitrineImoveis.Web.Models
 {
-    [Key]
-    public int Id { get; set; }
-    [Required]
-    [StringLength(150)]
-    public string Nome { get; set; } = null!;
-    [Required]
-    [EmailAdress]
-    public string Email { get; set; } = null!;
-    [StringLength(20)]
+    public enum OrigemLead { FormularioContatoGeral, PaginaDeImovel, Outro }
 
-    public string? Telefone { get; set; }
-    public string? Mensagem { get; set; }
+    public class Lead
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(150)]
+        public string Nome { get; set; } = null!;
+        
+        [Required]
+        [EmailAddress] // CORREÇÃO 2: O correto é 'EmailAddress' com dois 'd'
+        public string Email { get; set; } = null!;
+        
+        [StringLength(20)]
+        public string? Telefone { get; set; }
+        
+        public string? Mensagem { get; set; }
+        
+        public DateTime DataContato { get; set; } = DateTime.UtcNow;
+        
+        [Required]
+        public OrigemLead Origem { get; set; }
 
-    public DateTime DataContato { get; set; } = DateTime.UtcNow;
-    [Required]
-    public OrigemLead Origem { get; set; } // Campo mantido para análise
-
-    [ForeignKey("Imovel")]
-    public int? ImovelId { get; set; } 
-    public virtual Imovel? Imovel { get; set; }
+        [ForeignKey("Imovel")]
+        public int? ImovelId { get; set; }
+        public virtual Imovel? Imovel { get; set; }
+    }
 }
